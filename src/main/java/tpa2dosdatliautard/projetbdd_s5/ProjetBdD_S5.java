8eb828bd27c34185da0205150056cd4e3b7fdca4
cliautard <liautard.clotilde@gmail.com>
2022-10-05 08:58:51 +0200

    etat initial

--- src/main/java/tpa2dosdatliautard/projetbdd_s5/ProjetBdD_S5.java
+++ src/main/java/tpa2dosdatliautard/projetbdd_s5/ProjetBdD_S5.java
@@ -12,6 +12,6 @@ package tpa2dosdatliautard.projetbdd_s5;
 public class ProjetBdD_S5 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello");
     }
 }
