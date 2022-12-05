fd2b8bbca2e8095d0a7f886948a18b1b5c1ceeef
Cloclo <81481047+Clotilde636@users.noreply.github.com>
2022-12-05 14:55:12 +0100

    Update Administrateur.java

--- src/main/java/tpa2dosdatliautard/projetbdd_s5/Administrateur.java
+++ src/main/java/tpa2dosdatliautard/projetbdd_s5/Administrateur.java
@@ -9,8 +9,8 @@ public class Administrateur extends Personne {
     private int randomVariable; //modèle à suivre pour rajouter des paramètres pour Administrateur, en plus de celles déjà créées par la superclasse Personne
             
 //Constructeur
-    public Administrateur (String email,String pass,String nom,String prenom, int codePostal,int randomValue){
-        super(email,pass,nom,prenom,codePostal); //appelle le constructeur de la superclasse Personne
+    public Administrateur (String email,String pass,int randomValue){
+        super(email,pass); //appelle le constructeur de la superclasse Personne
         this.randomVariable=randomValue; //rajout de paramètres par rapport à la superclasse Personne
     }
     
@@ -21,6 +21,108 @@ public class Administrateur extends Personne {
     } 
      public void set_nombreEncheres(int newRandomVariable) {
         this.randomVariable = newRandomVariable;
+    }
+     
+    public static void DeleteUtilisateur (Connection con, String email) throws SQLException{ 
+        con.setAutoCommit(false);
+        try{
+            try ( PreparedStatement pst = con.prepareStatement(
+                    "delete from personne where email = ?")) {
+                pst.setString(1, email);
+                pst.executeUpdate();
+            }
+            con.commit();
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }    
+    }
+    
+    public static void DeleteArticle (Connection con, int idArticle) throws SQLException{
+        con.setAutoCommit(false);
+        try{
+            try ( PreparedStatement pst = con.prepareStatement(
+                    "delete from articles where idArticle = ?")) {
+                pst.setInt(1, idArticle);
+                pst.executeUpdate();
+            }
+            con.commit();
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }    
+    }
+    
+    public static void DeleteArticleParDeleteUtilisateur (Connection con, String email) throws SQLException{
+        con.setAutoCommit(false);
+        try{
+            try ( PreparedStatement pst = con.prepareStatement(
+                    "delete from articles where emailVendeur = ?")) {
+                pst.setString(1, email);
+                pst.executeUpdate();
+            }
+            con.commit();
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }    
+    }
+    
+    public static void DeleteEnchere (Connection con, int idEnchere) throws SQLException{  
+        con.setAutoCommit(false);
+        try{
+            try ( PreparedStatement pst = con.prepareStatement(
+                    "delete from encheres where idEnchere = ?")) {
+                pst.setInt(1, idEnchere);
+                pst.executeUpdate();
+            }
+            con.commit();
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }
+    }
+    
+    public static void DeleteEnchereParDeleteArticle (Connection con, int idArticle) throws SQLException{  
+        con.setAutoCommit(false);
+        try{
+            try ( PreparedStatement pst = con.prepareStatement(
+                    "delete from encheres where idArticle = ?")) {
+                pst.setInt(1, idArticle);
+                pst.executeUpdate();
+            }
+            con.commit();
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }    
+    }
+    
+    public static void DeleteEnchereParDeleteUtilisateur (Connection con, String email) throws SQLException{
+        con.setAutoCommit(false);
+        try{
+            try ( PreparedStatement pst = con.prepareStatement(
+                    "delete from encheres where emailVendeur = ?")) {
+                pst.setString(1, email);
+                pst.executeUpdate();
+            }
+            con.commit();
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }
     }
     
 }
